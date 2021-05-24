9535d35fd62c944adc27b6073016075ed68136a1
Arno van Staden <arnovanstaden@gmail.com>
2021-05-24 16:49:59 +0200

    add section

--- components/UI/Section/Section.tsx
+++ components/UI/Section/Section.tsx
@@ -1,10 +1,40 @@
+import ClassNames from "classnames"
+
+// Components
+import Container from "../Library/Container/Container";
+
 // Styles
 import styles from "./section.module.scss";
 
-const Section = () => {
-    return (
-        <section className={styles.section}>
+interface IProps {
+    children: React.ReactNode;
+    heading: React.ReactChild;
+    reverse?: boolean;
+    keywords: string;
+}
 
+const Section = (props: IProps) => {
+
+    const classes = ClassNames(
+        styles.section,
+        props.reverse ? styles.reverse : null
+    )
+
+    return (
+        <section className={classes}>
+            <div className={styles.keywords}>
+                <p>{props.keywords}</p>
+            </div>
+            <Container>
+                <div className={styles.grid}>
+                    <div className={styles.heading}>
+                        {props.heading}
+                    </div>
+                    <div className={styles.content}>
+                        {props.children}
+                    </div>
+                </div>
+            </Container>
         </section>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 // Components
 import Page from "../components/UI/Page/Page";
 import Landing from "../components/UI/Landing/Landing";
+import Section from "../components/UI/Section/Section";
 
 // Styles
 import styles from "../styles/pages/index.module.scss";
@@ -15,10 +16,27 @@ const Home = () => {
       }}
       className={styles.home}
     >
+
       <Landing
         image="/images/pages/home/landing.jpeg">
         <h1>Online <span>learning platform</span> and competence management system</h1>
       </Landing>
+
+      <Section
+        heading={<h2><span>Who</span> are we?</h2>}
+        keywords="Who Are We?"
+      >
+        <h3>The D+MER Golden Thread</h3>
+        <p>
+          Our team firmly believe that Every member of a commercial dive operation must follow the golden thread to ensure an effective outcome in the event of a medical emergency. The Golden Thread ensures The Right People + The Right Equipment + The Right Training to ensure an Effective and Efficient Outcome for all in the event of a Medical Emergency.
+        </p>
+
+        <p>
+          D+MER Worldwide offers the Commercial Diving Community - Online Knowledge modules to stay in-date! Access to on-site Skills assessment and training centres globally. The right Equipment: DMAC 015 compliant medical kits, tailor made to suit your dive spread and needs.
+        </p>
+
+      </Section>
+
     </Page >
   )
 }
