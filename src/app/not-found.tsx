6e4e8d922c58a1a6a5051c50ae420e7bb2c18e19
arnovanstaden <arnovanstaden@gmail.com>
2023-09-18 08:46:09 +0200

    move 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,20 @@
 // Components
-import Page from "../components/UI/Page/Page";
-import Button from "../components/UI/Library/Button/Button";
+import Page from '@components/UI/Page/Page';
+import Button from '@components/UI/Library/Button/Button';
 
 // Styles
-import styles from "../styles/pages/404.module.scss";
+import styles from './styles.module.scss';
 
 const PageNotFound = () => {
   return (
     <Page
       head={{
-        title: "Page Not Found",
-        canonical: "/404",
-        description: "Page not found.",
+        title: 'Page Not Found',
+        canonical: '/404',
+        description: 'Page not found.',
         robots: false
       }}
-      className={styles.pageNotFound}
+      className={styles.PageNotFound}
     >
       <div className={styles.error}>
         <h1>4</h1>
