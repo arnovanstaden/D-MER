55577b52f1840abc04eee2602623c4ca6ccc9651
arnovanstaden <arnovanstaden@gmail.com>
2024-02-14 18:42:01 +0100

    Add sitemap

--- custom-next-sitemap.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteUrl: 'https://www.dmerworldwide.com',
-  generateRobotsTxt: true, // (optional),
-  exclude: ['/404', '/cart', '/cart*'],
-  changefreq: 'daily',
-  priority: 0.8,
-  sitemapSize: 7000,
-}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next';
+
+const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  const url = 'https://www.dmerwordwide.com';
+
+  const basicPages = ['', 'about', 'courses', 'courses/book', 'contact'];
+
+  const basicPagesSitemap = basicPages.map((page) => ({
+    url: `${url}/${page}`,
+    lastModified: new Date(),
+  }));
+
+  return basicPagesSitemap;
+}
+
+export default sitemap
